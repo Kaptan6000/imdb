ce0041e43e2806f5743c3e6f184e53c70d548ad0
Kaptan Singh <85995522+Kaptan6000@users.noreply.github.com>
2022-09-25 11:25:53 +0530

    Update imdb.js

--- imdb.js
+++ imdb.js
@@ -1,189 +1,394 @@
-/**
- * The URL prefix of the API endpoint with the API key generated for this purpsoe
- * @const {string}
- */
- const OMDB_API_URL = 'http://www.omdbapi.com/?apikey=31a6e817&';
+//TMDB 
 
- // Constants for storing the references to DOM elements I'll need later
- const searchForm = document.querySelector('form');
- const searchBox = document.querySelector('#search-box');
- const searchType = document.querySelector('#search-type');
- const resultsDiv = document.querySelector('#results');
- const realtimeSearchDiv = document.querySelector('#realtime-search');
- 
- /**
-  * Return the string value of available search types in OMDb's API
-  * based on the string that was passed in as an argument.
-  * @param {string} value
-  * @example
-  * // returns {'param' : 't', 'type' : 'm'}
-  * checkSearchType('titles');
-  * @returns {Object} Object with 2 keys 'Param' and 'Type' { 'param': 't | i | s', 'type': 'm | episode | series | imdbs | ""' }
-  */
- function checkSearchType(value) {
-     const obj = {};
-     if (value === 'titles') {
-         obj.param = 't';
-         obj.type = 'm';
-     } else if (value === 'episode') {
-         obj.param = 't';
-         obj.type = 'episode';
-     } else if (value === 'series') {
-         obj.param = 't';
-         obj.type = 'series';
-     } else if (value === 'imdbs') {
-         obj.param = 'i';
-         obj.type = '';
-     } else {
-         obj.param = 's';
-         obj.type = '';
-     }
-     return obj;
- }
- 
- /** Return the URL of the OMDb API endpoint to fetch from - based on users input */
- function buildURL() {
-     const params = checkSearchType(searchType.value);
-     return `${OMDB_API_URL}${params.param}=${searchBox.value}&type=${params.type}`;
- }
- 
- /** Return the URL of the OMDb API endpoint to fetch the random title - based on IMDb IDs */
- function buildRandomURL() {
-     const randomID = `tt0${Math.floor(Math.random() * 1000000)}`;
-     return `${OMDB_API_URL}i=${randomID}`;
- }
- 
-/*
-  * Return the DOM element with the passed in parammeters.
-  * @param {string} elem HTML element to be created.
-  * @param {string} content Depending on elem, this will either be img alt text, div classname or text to render on the page.
-  * @param {string} src Depending on elem, this will either be img src attribute or div id.
-  * @returns {DOMelement} DOM element
-  */
- function createElement(elem, content, src) {
-     const element = document.createElement(elem);
-     if (elem === 'img') {
-         element.setAttribute('src', src);
-         element.setAttribute('alt', content);
-         element.setAttribute('width', '200px');
-         element.setAttribute('height', '300px');
-     } else if (elem === 'div') {
-         element.classList.add(content);
-         // eslint-disable-next-line no-unused-expressions
-         (src) ? element.id = src : src;
-     } else {
-         element.innerText = content;
-     }
-     return element;
- }
- 
- /**
-  * This gets called whenever the user types something in the search box
-  * to update the DOM accordingly by creating elements on the page.
-  * @param {event} e Object of the caller event
-  */
- function realtimeSearch(e) {
-     const FULL_OMDB_URL = buildURL();
- 
-     fetch(FULL_OMDB_URL)
-         .then((res) => {
-             // check if the call was good and pass on the data
-             if (res.ok) {
-                 return res.json();
-             }
-             // or throw an error
-             throw new Error('Network response was not OK!');
-         })
-         .then((resp) => {
-             // The response was OK so check for results
-             if (resp.Response === 'True') {
-                 // The search results came back so
-                 // clear the realtime results container, show it...
-                 realtimeSearchDiv.innerText = '';
-                 realtimeSearchDiv.classList.remove('hidden');
-                 realtimeSearchDiv.classList.add('visible');
- 
-                 // and start filling in the realtime results container with the data:
-                 // create the header with info
-                 realtimeSearchDiv.appendChild(createElement('h3', `Number of results: ${resp.totalResults}`));
-             } else if (resp.Response === 'False') {
-                 // There are no search results so hide the realtime search container
-                 realtimeSearchDiv.classList.add('hidden');
-                 realtimeSearchDiv.classList.remove('visible');
-             }
-         })
-         .catch(err => console.error(`Event ${e} caused an error`, err));
- }
- 
- /**
-  * This fires when a user has committed the search either by clicking on a search button,
-  * or by pressing enter/return key.
-  * @param {event} e Object of the caller event.
-  * @param {string} value (optional) The full OMDb APIs enpoint URL to fetch.
-  */
- function startTheSearch(e, value) {
-     if (e) e.preventDefault();
-     console.log(e);
- 
-     const FULL_OMDB_URL = value || buildURL();
- 
-     fetch(FULL_OMDB_URL)
-         .then((res) => {
-             // check if the call was good and pass on the data
-             if (res.ok) {
-                 return res.json();
-             }
-             // or throw an error
-             throw new Error('Network response was not OK!');
-         })
-         .then((resp) => {
-             // The response was OK so check for results
-             if (resp.Response === 'True') {
-                 // The search results came back so
-                 // clear the results container, show it...
-                 resultsDiv.innerText = '';
-                 resultsDiv.style.visibility = 'visible';
- 
-                 // then hide and clear realtime search container...
-                 realtimeSearchDiv.classList.add('hidden');
-                 realtimeSearchDiv.classList.remove('visible');
-                 realtimeSearchDiv.innerText = '';
- 
-                 // and start filling in the results container with the data:
-                 // create the header with info
-                 if (resp.totalResults) {
-                     resultsDiv.appendChild(createElement('h3', `Number of results: ${resp.totalResults}`));
-                 }
-                 // create the div for movie results...
-                 resultsDiv.appendChild(createElement('div', 'movie-results'));
-                 const movieResults = document.querySelector('.movie-results');
- 
-                 if (resp.Search) {
-                     // loop through results and for each movie - create a div and populate it with data.
-                     let i = 0;
-                     resp.Search.forEach((movie) => {
-                         movieResults.appendChild(createElement('div', 'movie-result', `movie-result-${i}`));
-                         const movieResult = document.querySelector(`#movie-result-${i}`);
-                         movieResult.appendChild(createElement('img', movie.Title, movie.Poster));
-                         movieResult.appendChild(createElement('p', movie.Title));
-                         i += 1;
-                     });
-                 } else {
-                     movieResults.appendChild(createElement('div', 'movie-result', 'movie-result'));
-                     const movieResult = document.querySelector('#movie-result');
-                     movieResult.appendChild(createElement('img', resp.Title, resp.Poster));
-                     movieResult.appendChild(createElement('p', resp.Title));
-                 }
-             }
-         })
-         .catch(err => console.error(err));
- }
- 
- // Whenever a change is made inside the Search input box on a page
- searchBox.addEventListener('input', realtimeSearch);
- 
- // When a user has submitted the search request
- searchForm.addEventListener('submit', startTheSearch);
- 
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+var selectedGenre = []
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML= '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            }else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length !=0){   
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
+}
+
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+            
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();            
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+    
+}
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+
+            if(currentPage <= 1){
+              prev.classList.add('disabled');
+              next.classList.remove('disabled')
+            }else if(currentPage>= totalPages){
+              prev.classList.remove('disabled');
+              next.classList.add('disabled')
+            }else{
+              prev.classList.remove('disabled');
+              next.classList.remove('disabled')
+            }
+
+            tagsEl.scrollIntoView({behavior : 'smooth'})
+
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+       
+    })
+
+}
 
 
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(movie)
+        })
+    })
+}
+
+const overlayContent = document.getElementById('overlay-content');
+/* Open when someone clicks on the span element */
+function openNav(movie) {
+  let id = movie.id;
+  fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json()).then(videoData => {
+    console.log(videoData);
+    if(videoData){
+      document.getElementById("myNav").style.width = "100%";
+      if(videoData.results.length > 0){
+        var embed = [];
+        var dots = [];
+        videoData.results.forEach((video, idx) => {
+          let {name, key, site} = video
+
+          if(site == 'YouTube'){
+              
+            embed.push(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          `)
+
+            dots.push(`
+              <span class="dot">${idx + 1}</span>
+            `)
+          }
+        })
+        
+        var content = `
+        <h1 class="no-results">${movie.original_title}</h1>
+        <br/>
+        
+        ${embed.join('')}
+        <br/>
+
+        <div class="dots">${dots.join('')}</div>
+        
+        `
+        overlayContent.innerHTML = content;
+        activeSlide=0;
+        showVideos();
+      }else{
+        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+      }
+    }
+  })
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+var activeSlide = 0;
+var totalVideos = 0;
+
+function showVideos(){
+  let embedClasses = document.querySelectorAll('.embed');
+  let dots = document.querySelectorAll('.dot');
+
+  totalVideos = embedClasses.length; 
+  embedClasses.forEach((embedTag, idx) => {
+    if(activeSlide == idx){
+      embedTag.classList.add('show')
+      embedTag.classList.remove('hide')
+
+    }else{
+      embedTag.classList.add('hide');
+      embedTag.classList.remove('show')
+    }
+  })
+
+  dots.forEach((dot, indx) => {
+    if(activeSlide == indx){
+      dot.classList.add('active');
+    }else{
+      dot.classList.remove('active')
+    }
+  })
+}
+
+const leftArrow = document.getElementById('left-arrow')
+const rightArrow = document.getElementById('right-arrow')
+
+leftArrow.addEventListener('click', () => {
+  if(activeSlide > 0){
+    activeSlide--;
+  }else{
+    activeSlide = totalVideos -1;
+  }
+
+  showVideos()
+})
+
+rightArrow.addEventListener('click', () => {
+  if(activeSlide < (totalVideos -1)){
+    activeSlide++;
+  }else{
+    activeSlide = 0;
+  }
+  showVideos()
+})
+
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre=[];
+    setGenre();
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
+prev.addEventListener('click', () => {
+  if(prevPage > 0){
+    pageCall(prevPage);
+  }
+})
+
+next.addEventListener('click', () => {
+  if(nextPage <= totalPages){
+    pageCall(nextPage);
+  }
+})
+
+function pageCall(page){
+  let urlSplit = lastUrl.split('?');
+  let queryParams = urlSplit[1].split('&');
+  let key = queryParams[queryParams.length -1].split('=');
+  if(key[0] != 'page'){
+    let url = lastUrl + '&page='+page
+    getMovies(url);
+  }else{
+    key[1] = page.toString();
+    let a = key.join('=');
+    queryParams[queryParams.length -1] = a;
+    let b = queryParams.join('&');
+    let url = urlSplit[0] +'?'+ b
+    getMovies(url);
+  }
+}
